7e214de1d87829aa0c34020dffa28e4372768ae7
Mubelotix <mubelotix@gmail.com>
2022-10-09 17:21:36 +0200

    Format to yew

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 use std::collections::HashMap;
 
-use html5ever::{tokenizer::{Tokenizer, TokenizerOpts, TokenSink, Token as HtmlToken, BufferQueue, TokenSinkResult, Doctype}, buffer_queue};
+use html5ever::{tokenizer::{Tokenizer, TokenizerOpts, TokenSink, Token as HtmlToken, BufferQueue, TokenSinkResult, Doctype, Tag, TagKind}, buffer_queue, Attribute};
 use proc_macro::{TokenStream, TokenTree};
 use string_tools::get_all_between_strict;
 
@@ -64,17 +64,47 @@ fn parse_args(args: TokenStream) -> Args {
 
 struct HtmlSink<'a> {
     html_tokens: &'a mut Vec<HtmlToken>,
+    current_text: String,
 }
 
 impl<'a> TokenSink for HtmlSink<'a> {
     type Handle = ();
 
-    fn process_token(&mut self, token: HtmlToken, _line_number: u64) -> TokenSinkResult<()> {
-        match token {
-            HtmlToken::DoctypeToken(_) => (),
-            token => self.html_tokens.push(token)
-        };
+    fn process_token(&mut self, token: HtmlToken, line_number: u64) -> TokenSinkResult<()> {
+        if let HtmlToken::ParseError(e) = token {
+            panic!("Failed to parse template: {e} at line {line_number}");
+        }
+        if let HtmlToken::CharacterTokens(new_text) = token {
+            self.current_text.push_str(&new_text);
+        } else {
+            let mut old_text = std::mem::take(&mut self.current_text);
+            old_text = old_text.trim().to_string();
+            if !old_text.is_empty() {
+                self.html_tokens.push(HtmlToken::CharacterTokens(old_text.into()));
+            }
+            self.html_tokens.push(token);
+        }
         TokenSinkResult::Continue
+    }
+}
+
+fn attr_to_yew_string(attr: Attribute) -> String {
+    format!("{}=\"{}\"", attr.name.local, attr.value) // TODO
+}
+
+fn html_token_to_yew_string(token: HtmlToken) -> String {
+    match token {
+        HtmlToken::TagToken(Tag { kind, name, self_closing, attrs }) => {
+            let f_attrs = attrs.into_iter().map(attr_to_yew_string).collect::<Vec<_>>().join(" ");
+            match kind {
+                TagKind::StartTag if self_closing => format!("<{name} {f_attrs}/>"),
+                TagKind::StartTag => format!("<{name} {f_attrs}>"),
+                TagKind::EndTag => format!("</{name} {f_attrs}>"),
+            }
+        }
+        HtmlToken::CharacterTokens(text) => format!("{{\"{}\"}}", text),
+        HtmlToken::DoctypeToken(_) | HtmlToken::CommentToken(_) | HtmlToken::NullCharacterToken | HtmlToken::EOFToken => String::new(),
+        HtmlToken::ParseError(_) => unreachable!(),
     }
 }
 
@@ -84,7 +114,7 @@ fn generate_code(args: Args) -> String {
         Err(e) => panic!("Failed to read template file at {}: {}", args.path, e),
     };
     let mut html_tokens = Vec::new();
-    let html_sink = HtmlSink { html_tokens: &mut html_tokens };
+    let html_sink = HtmlSink { html_tokens: &mut html_tokens, current_text: String::new() };
     let mut html_tokenizer = Tokenizer::new(html_sink, TokenizerOpts::default());
     let mut buffer_queue = BufferQueue::new();
     buffer_queue.push_back(template.clone().into());
@@ -92,6 +122,8 @@ fn generate_code(args: Args) -> String {
     html_tokenizer.end();
 
     println!("{:#?}", html_tokens);
+    let yew_text = html_tokens.into_iter().map(html_token_to_yew_string).collect::<Vec<_>>().join("");
+    println!("{}", yew_text);
 
     while let Some(to_replace) = get_all_between_strict(&template, "{", "}").map(|s| s.to_string()) {
         if to_replace.chars().any(|c| !c.is_alphanumeric() && c != '_') {
