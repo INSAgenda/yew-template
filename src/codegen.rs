66325d6bcb2ed53f9efcf6326949a36daff294bd
Mubelotix <mubelotix@gmail.com>
2022-10-10 00:52:42 +0200

    Change structure

--- src/codegen.rs
+++ src/codegen.rs
@@ -3,9 +3,7 @@ use proc_macro::TokenTree;
 use string_tools::get_all_between_strict;
 use crate::*;
 
-fn attr_to_yew_string(attr: Attribute, args: &Args) -> String {
-    let name = attr.name.local.to_string();
-    let value = attr.value.to_string();
+fn attr_to_yew_string((name, value): (String, String), args: &Args) -> String {
     if value.starts_with('[') && value.ends_with(']') {
         let id = value[1..value.len() - 1].to_string();
         match args.vals.get(&id) {
@@ -27,18 +25,27 @@ fn attr_to_yew_string(attr: Attribute, args: &Args) -> String {
     }
 }
 
-fn html_token_to_yew_string(token: HtmlToken, args: &Args) -> String {
-    match token {
-        HtmlToken::TagToken(Tag { kind, name, self_closing, attrs }) => {
-            let f_attrs = attrs.into_iter().map(|a| attr_to_yew_string(a, args)).collect::<Vec<_>>().join(" ");
-            match kind {
-                TagKind::StartTag if self_closing => format!("<{name} {f_attrs}/>"),
-                TagKind::StartTag => format!("<{name} {f_attrs}>"),
-                TagKind::EndTag => format!("</{name} {f_attrs}>"),
+fn html_part_to_yew_string(part: HtmlPart, args: &Args) -> String {
+    match part {
+        HtmlPart::Element(element) => {
+            if element.self_closing && !element.close_attrs.is_empty() {
+                panic!("Self-closing tags cannot have closing attributes");
+            }
+            if element.self_closing && !element.children.is_empty() {
+                panic!("Self-closing tags cannot have children");
+            }
+
+            let f_open_attrs = element.open_attrs.into_iter().map(|a| attr_to_yew_string(a, args)).collect::<Vec<_>>().join(" ");
+            let f_close_attrs = element.close_attrs.into_iter().map(|a| attr_to_yew_string(a, args)).collect::<Vec<_>>().join(" ");
+            let name = element.name;
+            let content = element.children.into_iter().map(|p| html_part_to_yew_string(p, args)).collect::<Vec<_>>().join("");
+
+            match element.self_closing {
+                true => format!("<{name} {f_open_attrs}/>"),
+                false => format!("<{name} {f_open_attrs}>{content}</{name} {f_close_attrs}>"),
             }
         }
-        HtmlToken::CharacterTokens(text) => {
-            let mut text = text.to_string();
+        HtmlPart::Text(mut text) => {
             while let Some(to_replace) = get_all_between_strict(&text, "[", "]").map(|s| s.to_string()) {
                 if to_replace.chars().any(|c| !c.is_alphanumeric() && c != '_') {
                     panic!("Invalid identifier: {to_replace:?} in template {}", args.path);
@@ -52,9 +59,7 @@ fn html_token_to_yew_string(token: HtmlToken, args: &Args) -> String {
             text = text.replace("{\"\"}", "");
 
             text
-        },
-        HtmlToken::DoctypeToken(_) | HtmlToken::CommentToken(_) | HtmlToken::NullCharacterToken | HtmlToken::EOFToken => String::new(),
-        HtmlToken::ParseError(_) => unreachable!(),
+        }
     }
 }
 
@@ -63,18 +68,26 @@ pub(crate) fn generate_code(args: Args) -> String {
         Ok(template) => template,
         Err(e) => panic!("Failed to read template file at {}: {}", args.path, e),
     };
-    let mut html_tokens = Vec::new();
-    let html_sink = HtmlSink { html_tokens: &mut html_tokens, current_text: String::new() };
+    let mut html_parts = Vec::new();
+    let html_sink = HtmlSink { html_parts: &mut html_parts, opened_elements: Vec::new() };
     let mut html_tokenizer = Tokenizer::new(html_sink, TokenizerOpts::default());
     let mut buffer_queue = BufferQueue::new();
     buffer_queue.push_back(template.into());
     let _  = html_tokenizer.feed(&mut buffer_queue);
     html_tokenizer.end();
-    //println!("{:#?}", html_tokens);
+    let mut root = Element {
+        name: "".to_string(),
+        open_attrs: Vec::new(),
+        close_attrs: Vec::new(),
+        self_closing: false,
+        children: html_parts,
+    };
+    root.clean_text();
+    println!("{:#?}", root);
 
-    let yew_html = html_tokens.into_iter().map(|t| html_token_to_yew_string(t, &args)).collect::<Vec<_>>().join("");
-    let yew_code = format!("html! {{ <> {yew_html} </> }}");
-    //println!("{}", yew_code);
+    let yew_html = html_part_to_yew_string(HtmlPart::Element(root), &args);
+    let yew_code = format!("html! {{ {yew_html} }}");
+    println!("{}", yew_code);
 
     yew_code
 }
--- src/sink.rs
+++ src/sink.rs
@@ -1,26 +1,91 @@
-use html5ever::{tokenizer::{TokenSink, Token as HtmlToken, TokenSinkResult}};
+use html5ever::{tokenizer::{TokenSink, Token as HtmlToken, TokenSinkResult, Tag, TagKind}};
+
+#[derive(Debug)]
+pub(crate) enum HtmlPart {
+    Text(String),
+    Element(Element),
+}
+
+#[derive(Debug)]
+pub(crate) struct Element {
+    pub(crate) name: String,
+    pub(crate) self_closing: bool,
+    pub(crate) open_attrs: Vec<(String, String)>,
+    pub(crate) close_attrs: Vec<(String, String)>,
+    pub(crate) children: Vec<HtmlPart>,
+}
+
+impl Element {
+    pub(crate) fn clean_text(&mut self) {
+        let mut new_children = Vec::new();
+        let mut current_text = String::new();
+        for child in self.children.drain(..) {
+            match child {
+                HtmlPart::Text(text) => {
+                    current_text.push_str(&text);
+                }
+                HtmlPart::Element(mut element) => {
+                    if !current_text.is_empty() {
+                        new_children.push(HtmlPart::Text(current_text));
+                        current_text = String::new();
+                    }
+                    element.clean_text();
+                    new_children.push(HtmlPart::Element(element));
+                }
+            }
+        }
+        if !current_text.is_empty() {
+            new_children.push(HtmlPart::Text(current_text));
+        }
+        self.children = new_children;
+    }
+}
 
 pub(crate) struct HtmlSink<'a> {
-    pub(crate) html_tokens: &'a mut Vec<HtmlToken>,
-    pub(crate) current_text: String,
+    pub(crate) html_parts: &'a mut Vec<HtmlPart>,
+    pub(crate) opened_elements: Vec<Element>,
 }
 
 impl<'a> TokenSink for HtmlSink<'a> {
     type Handle = ();
 
     fn process_token(&mut self, token: HtmlToken, line_number: u64) -> TokenSinkResult<()> {
-        if let HtmlToken::ParseError(e) = token {
-            panic!("Failed to parse template: {e} at line {line_number}");
-        }
-        if let HtmlToken::CharacterTokens(new_text) = token {
-            self.current_text.push_str(&new_text);
-        } else {
-            let mut old_text = std::mem::take(&mut self.current_text);
-            old_text = old_text.trim().to_string();
-            if !old_text.is_empty() {
-                self.html_tokens.push(HtmlToken::CharacterTokens(old_text.into()));
-            }
-            self.html_tokens.push(token);
+        match token {
+            HtmlToken::TagToken(tag) => match tag.kind {
+                TagKind::StartTag => {
+                    let element = Element {
+                        name: tag.name.to_string(),
+                        self_closing: tag.self_closing,
+                        open_attrs: tag.attrs.into_iter().map(|a| (a.name.local.to_string(), a.value.to_string())).collect(),
+                        close_attrs: Vec::new(),
+                        children: Vec::new(),
+                    };
+                    match element.self_closing {
+                        true => match self.opened_elements.last_mut() {
+                            Some(container) => container.children.push(HtmlPart::Element(element)),
+                            None => self.html_parts.push(HtmlPart::Element(element)),
+                        },
+                        false => self.opened_elements.push(element)
+                    }
+                },
+                TagKind::EndTag => {
+                    let mut element = self.opened_elements.pop().unwrap_or_else(|| panic!("Unexpected closing tag {} at line {line_number}", tag.name));
+                    if tag.name != element.name {
+                        panic!("Unexpected closing tag {} at line {line_number}", tag.name);
+                    }
+                    element.close_attrs = tag.attrs.into_iter().map(|a| (a.name.local.to_string(), a.value.to_string())).collect();
+                    match self.opened_elements.last_mut() {
+                        Some(container) => container.children.push(HtmlPart::Element(element)),
+                        None => self.html_parts.push(HtmlPart::Element(element)),
+                    }
+                },
+            },
+            HtmlToken::CharacterTokens(text) => match self.opened_elements.last_mut() {
+                Some(container) => container.children.push(HtmlPart::Text(text.to_string())),
+                None => self.html_parts.push(HtmlPart::Text(text.to_string())),
+            },
+            HtmlToken::NullCharacterToken | HtmlToken::CommentToken(_) | HtmlToken::EOFToken | HtmlToken::DoctypeToken(_) => (),
+            HtmlToken::ParseError(e) => panic!("Failed to parse template: {e} at line {line_number}"),
         }
         TokenSinkResult::Continue
     }
